eda82d63e775e21dc3bfa4b8504cde79bf30fdd9
ravi mishra <42548254+raviumishra@users.noreply.github.com>
2021-02-21 20:40:22 +0000

    test

--- 08_aks_acr/backend.tf
+++ 08_aks_acr/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
   resource_group_name  = "CMT"
   storage_account_name = "cmtterraformstate"
-  container_name       = "tfstate"
+  container_name       = "cmttfstate"
   key                  = "terraform.tfstate"
   }
 }
